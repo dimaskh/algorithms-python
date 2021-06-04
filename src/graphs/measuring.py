38cabead9f4159b1cd7a2f7b692ed4f9e7be4570
dimaskh <dimaskh.dev@gmail.com>
2021-06-04 13:54:05 +0300

    Add graph measuring

---
+++ src/graphs/measuring.py
@@ -0,0 +1,22 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+AGraph = nx.Graph()
+
+Nodes = range(1, 5)
+Edges = [(1, 2), (2, 3), (3, 4), (4, 5), (1, 3), (1, 5)]
+
+AGraph.add_nodes_from(Nodes)
+AGraph.add_edges_from(Edges)
+
+AGraph.add_node(6)
+print(sorted(nx.connected_components(AGraph)))
+
+AGraph.add_edge(1, 6)
+print(sorted(nx.connected_components(AGraph)))
+
+degrees = [val for (node, val) in nx.degree(AGraph)]
+print("Degrees: ", degrees)
+
+clustering = nx.clustering(AGraph)
+print("Clustering: ", clustering)
